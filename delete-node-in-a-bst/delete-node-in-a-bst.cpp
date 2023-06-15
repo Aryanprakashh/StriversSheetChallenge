35fc0b7f8e3e3ce4f1ac17f3abd0acd8e8b63317
Aryanprakashh <95036855+Aryanprakashh@users.noreply.github.com>
2023-06-15 22:46:11 +0530

    Time: 35 ms (79.20%) | Memory: 32.6 MB (57.49%) - LeetSync

---
+++ delete-node-in-a-bst/delete-node-in-a-bst.cpp
@@ -0,0 +1,59 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * }; 
+ */
+class Solution {
+public:
+    TreeNode *minval(TreeNode*root){
+        
+        while(root->left)
+                root=root->left;
+        return root;
+    }
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if(root==NULL)
+        return root;
+
+        if(root->val==key){
+            //0 chhild 
+            if(root->left==NULL and root->right==NULL){
+                delete root;
+                return NULL;
+            }
+            //1 child
+            //left child
+            if(root->left!=NULL and root->right==NULL){
+                TreeNode *temp=root->left;
+                delete root;
+                return temp;
+            }
+            //right child
+            if(root->left==NULL and root->right!=NULL){
+                TreeNode *temp=root->right;
+                delete root;
+                return temp;
+            }
+            //2 child
+            if(root->left!=NULL and root->right!=NULL){
+               int mini=minval(root->right)->val;
+                root->val=mini; 
+                root->right=deleteNode(root->right,mini);
+                return root;
+            }
+        }
+        else if(root->val>key){
+            root->left=deleteNode(root->left,key);
+            return root;
+        }
+        else 
+            root->right=deleteNode(root->right,key);
+            return root;
+    }
+};
