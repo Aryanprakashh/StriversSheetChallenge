0f05494b238af5812ad2c60be499410aef14fdd8
Aryanprakashh <95036855+Aryanprakashh@users.noreply.github.com>
2023-08-09 22:57:59 +0530

    Time: 134 ms (91.40%) | Memory: 81.2 MB (38.62%) - LeetSync

---
+++ 2720-minimize-the-maximum-difference-of-pairs/minimize-the-maximum-difference-of-pairs.cpp
@@ -0,0 +1,27 @@
+class Solution {
+    int countPairs(vector<int>& nums, int p, int diff){
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]-nums[i-1]<=diff){
+                i++;
+                p--;
+            }
+        }
+        return p<=0;
+    }
+public:
+    int minimizeMax(vector<int>& nums, int p) {
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        int left=0,right=nums[n-1]-nums[0],res=0;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            if(countPairs(nums,p,mid)){
+                res=mid;
+                right=mid-1;
+            } else {
+                left=mid+1;
+            }
+        }
+        return res;
+    }
+};
