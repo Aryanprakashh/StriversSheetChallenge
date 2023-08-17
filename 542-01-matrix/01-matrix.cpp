0adb999312b520fcae94637d9b3ee89741603cc5
Aryanprakashh <95036855+Aryanprakashh@users.noreply.github.com>
2023-08-17 11:10:10 +0530

    Time: 73 ms (75.81%) | Memory: 34.3 MB (42.54%) - LeetSync

---
+++ 542-01-matrix/01-matrix.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int n=mat.size();
+        int m=mat[0].size();
+        vector<vector<int>> vis(n,vector<int>(m,0));
+        vector<vector<int>> dis(n,vector<int>(m,0));
+
+        queue<pair<pair<int,int>, int>> q;
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+
+                if(mat[i][j]==0) {
+                    q.push({{i,j},0});
+                    vis[i][j]=1;
+                }
+                else vis[i][j]=0;
+            }
+        }
+
+        int delrow[]={-1,0,+1,0};
+        int delcol[]={0,+1,0,-1};
+        while(!q.empty()){
+            int row=q.front().first.first;
+            int col=q.front().first.second;
+            int step=q.front().second;
+            dis[row][col]=step;
+            q.pop();
+
+            for(int i=0;i<4;i++){
+                int nrow=delrow[i]+row;
+                int ncol=delcol[i]+col;
+
+                if(nrow>=0 and nrow<n and ncol>=0 and ncol<m and vis[nrow][ncol]==0){
+                    vis[nrow][ncol]=1;
+                    q.push({{nrow,ncol},step+1});
+                }
+            }
+        }
+        return dis;
+    }
+};
