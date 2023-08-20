2d13a827d2a90ea7a85b7db74011527fba8f10a6
Aryanprakashh <95036855+Aryanprakashh@users.noreply.github.com>
2023-08-20 16:52:44 +0530

    Updated Solution - GfG to GitHub

--- Medium/Number of occurrence/number-of-occurrence.cpp
+++ Medium/Number of occurrence/number-of-occurrence.cpp
@@ -8,51 +8,16 @@ using namespace std;
 //User function template for C++
 class Solution{
 public:	
-int firstoccur(int arr[],int size,int key){
-    int start=0;
-    int end=size-1;
-    int ans=0;
-    while(start<=end){
-        int mid=start+(end-start)/2;
-        if(arr[mid]==key){
-            ans= mid;
-            end=mid-1;
-        }
-        else if(arr[mid]<key){
-            start=mid+1;
-        }
-        else{
-            end=mid-1;
-        }
-    }
-    return ans;
-}
-int lastoccur(int arr[],int size,int key){
-    int start=0;
-    int end=size-1;
-    int ans=-1;
-    while(start<=end){
-        int mid=start+(end-start)/2;
-        if(arr[mid]==key){
-            ans= mid;
-            start=mid+1;
-        }
-        else if(arr[mid]<key){
-            start=mid+1;
-        }
-        else{
-            end=mid-1;
-        }
-    }
-    return ans;
-}
 	/* if x is present in arr[] then returns the count
 		of occurrences of x, otherwise returns 0. */
 	int count(int arr[], int n, int x) {
 	    // code here
-        int first=firstoccur(arr,n,x);
-        int last=lastoccur(arr,n,x);
-         return last-first+1;
+	    unordered_map<int,int> mp;
+	    
+	    for(int i=0;i<n;i++){
+	        mp[arr[i]]++;
+	    }
+	    return mp[x];
 	}
 };
 
