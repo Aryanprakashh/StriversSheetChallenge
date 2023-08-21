950b1f7e0dc75cafbcf4cc7dcf001214cecb84d6
Aryanprakashh <95036855+Aryanprakashh@users.noreply.github.com>
2023-08-21 23:14:16 +0530

    Time: 7 ms (79.49%) | Memory: 7.3 MB (46.57%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,9 +1,16 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        if(s==t) return true;
-        else return false;
+        unordered_map<char,int> mp;
+        for(auto i:s){
+            mp[i]++;
+        }
+        for(auto i:t){
+            mp[i]--;
+        }
+        for(auto i:mp){
+            if(i.second!=0) return false;
+        }
+        return true;
     }
 };
