53ac1754665f8571e811b9314d9ee0d8f6100661
Aryanprakashh <95036855+Aryanprakashh@users.noreply.github.com>
2023-09-20 14:31:51 +0530

    Time: 8 ms (80.20%) | Memory: 19.3 MB (14.60%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
-vector<vector<int>> dp;
-int func(int ind,vector<int> &coins,int amount){
-        if(amount==0) return 1;
-
-        if(ind < 0 || amount < 0 ) return 0;
-
-        if(dp[ind][amount]!=-1) return dp[ind][amount];
-        int notake=func(ind-1,coins,amount);
-        int take=func(ind,coins,amount-coins[ind]);
-        return dp[ind][amount]=take+notake;
+int solve(int ind,int amount,vector<int> &coins,vector<vector<int>> &dp){
+    if(ind==0) {
+        return (amount%coins[0]==0);
     }
+    // if(amount==0) return 1;
+    if(dp[ind][amount]!=-1) return dp[ind][amount];
+
+    int nottake=solve(ind-1,amount,coins,dp);
+    int take=0;
+    if(coins[ind]<=amount){
+        take+=solve(ind,amount-coins[ind],coins,dp);
+    }
+    return dp[ind][amount]=take+nottake;
+}
     int change(int amount, vector<int>& coins) {
-        dp.resize(coins.size()+1,vector<int>(amount+1,-1));
-        return func(coins.size()-1,coins,amount);
+        int n=coins.size();
+        vector<vector<int>> dp(n+1,vector<int>(amount+1,-1));
+        return solve(n-1,amount,coins,dp);
     }
 };
