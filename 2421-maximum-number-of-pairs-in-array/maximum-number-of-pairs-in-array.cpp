8ff18d8e96fc4e52fac07cce04359324ebeb929c
Aryanprakashh <95036855+Aryanprakashh@users.noreply.github.com>
2024-08-02 18:46:43 +0530

    Time: 6 ms (21.61%) | Memory: 11.6 MB (41.84%) - LeetSync

---
+++ 2421-maximum-number-of-pairs-in-array/maximum-number-of-pairs-in-array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> numberOfPairs(vector<int>& nums) {
+        unordered_map<int,int> mp;
+        for(auto i:nums){
+            mp[i]++;
+        }      
+
+        int pair = 0, leftover = 0;
+        for(auto i : mp) {
+            pair += i.second / 2;  // Count the number of pairs
+            leftover += i.second % 2;  // Count the leftovers
+        }
+        return {pair, leftover};
+    }
+};
